734a6ab62fb930f7eae9cd206dfa35ba31d805eb
Vladislav Kuznetsov <headgenrekty+github@gmail.com>
2025-02-09 15:16:44 +0400

    Fixed imports

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_ckeditor_5.fields import CKEditor5Field 
 
 # Create your models here.
 class Post(models.Model):
