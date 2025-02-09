51747a361a01a3aeaccc553a0e914d177a903da6
Vladislav Kuznetsov <headgenrekty+github@gmail.com>
2025-02-09 16:43:28 +0400

    Added top10

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 def index(req):
-    return render(req, 'home.html')
+        # Фиктивные данные для демонстрации
+    top_users = [
+        {'username': 'Иван Иванов', 'courses_completed': 15},
+        {'username': 'Анна Петрова', 'courses_completed': 12},
+        {'username': 'Сергей Сидоров', 'courses_completed': 9},
+        {'username': 'Елена Васильева', 'courses_completed': 7},
+        {'username': 'Дмитрий Кузнецов', 'courses_completed': 6},
+    ]
+    context = {
+        'top_users': top_users
+    }
+    return render(req, 'home.html', context)
 
 def about(req):
     return render(req, 'about.html')
